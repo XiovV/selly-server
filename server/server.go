485014a401332a90761d0bfec698cb90f2f24b10
XiovV <xiovv@tutanota.com>
2022-09-22 16:44:00 +0200

    setup for parsing different message types

--- server/server.go
+++ server/server.go
@@ -18,7 +18,8 @@ import (
 )
 
 const (
-	version = "0.1.1"
+	version     = "0.1.1"
+	messageType = "message"
 )
 
 type Server struct {
@@ -35,6 +36,11 @@ func New(hub *hub.Hub, mq *rabbitmq.RabbitMQ, redis *redis.Redis, logger *zap.Su
 	go s.consumeQueue()
 
 	return s
+}
+
+type Envelope struct {
+	Type string
+	Msg  any
 }
 
 func (s *Server) Serve() {
@@ -60,9 +66,11 @@ func (s *Server) Chat(w http.ResponseWriter, r *http.Request) {
 
 	sender := s.contextGetSender(r)
 
-	var message models.Message
+	var msg json.RawMessage
+	payload := Envelope{Msg: &msg}
+
 	for {
-		err := c.ReadJSON(&message)
+		err := c.ReadJSON(&payload)
 		if err != nil {
 			switch {
 			case errors.Is(err, io.ErrUnexpectedEOF):
@@ -80,32 +88,57 @@ func (s *Server) Chat(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		message.DateCrated = time.Now().Unix()
-
-		receiver, exists := s.hub.Get(message.Receiver)
-
-		if exists {
-			s.log.Debugw("receiver exists in the local hub, sending message directly to the user", "sender", message.Sender, "receiver", message.Receiver)
-
-			go s.sendMessage(message, receiver)
-		} else if s.redis.IsUserOnline(message.Receiver) {
-			s.log.Debugw("receiver does not exist in the local hub but is online, pushing message to RabbitMQ", "sender", message.Sender, "receiver", message.Receiver)
-			err = s.mq.Publish(message)
-			if err != nil {
-				s.log.Error("failed to publish a message:", err)
-			}
-		} else {
-			s.log.Debugw("user is offline, pushing message to redis", "sender", message.Sender, "receiver", message.Receiver)
-
-			s.redis.PushMessage(message)
+		switch payload.Type {
+		case messageType:
+			s.handleIncomingMessage(msg)
+		default:
+			log.Fatal("unknown message type")
 		}
 
-		s.log.Debugw("sending", "message", message)
 	}
 }
 
+func (s *Server) handleIncomingMessage(msg json.RawMessage) {
+	var message models.Message
+
+	if err := json.Unmarshal(msg, &message); err != nil {
+		s.log.Error("couldn't unmarshal message:", err)
+		return
+	}
+
+	message.DateCrated = time.Now().Unix()
+
+	receiver, exists := s.hub.Get(message.Receiver)
+
+	if exists {
+		s.log.Debugw("receiver exists in the local hub, sending message directly to the user", "sender", message.Sender, "receiver", message.Receiver)
+
+		go s.sendMessage(message, receiver)
+	} else if s.redis.IsUserOnline(message.Receiver) {
+		s.log.Debugw("receiver does not exist in the local hub but is online, pushing message to RabbitMQ", "sender", message.Sender, "receiver", message.Receiver)
+		err := s.mq.Publish(message)
+		if err != nil {
+			s.log.Error("failed to publish a message:", err)
+		}
+	} else {
+		s.log.Debugw("user is offline, pushing message to redis", "sender", message.Sender, "receiver", message.Receiver)
+
+		s.redis.PushMessage(message)
+	}
+
+	s.log.Debugw("sending", "message", message)
+}
+
 func (s *Server) sendMessage(msg models.Message, receiver *websocket.Conn) {
-	err := receiver.WriteJSON(msg)
+	var m struct {
+		Type string
+		Msg  models.Message
+	}
+
+	m.Type = "message"
+	m.Msg = msg
+
+	err := receiver.WriteJSON(m)
 	if err != nil {
 		s.log.Error("failed to send a message:", err)
 	}
